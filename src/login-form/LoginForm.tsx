b9a5dda8d64f836bce8587bd04861335529be20a
IgnacyBerent <2gb02ignac@gmail.com>
2024-04-29 14:07:38 +0200

    Improving Login page

--- src/login-form/LoginForm.tsx
+++ src/login-form/LoginForm.tsx
@@ -24,53 +24,64 @@ function LoginForm() {
   );
 
   return (
-    <Formik
-      initialValues={initialValues}
-      onSubmit={onSubmit}
-      validationSchema={validationSchema}
-      validateOnChange
-      validateOnBlur
-    >
-      {(formik: any) => (
-        <form
-          className="login-form"
-          id="signForm"
-          onSubmit={formik.handleSubmit}
-          noValidate
-        >
-          <TextField
-            id="username"
-            label="Username"
-            variant="standard"
-            name="username"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            error={formik.touched.username && !!formik.errors.username}
-            helperText={formik.touched.username && formik.errors.username}
-          />
-          <TextField
-            id="password"
-            label="Password"
-            variant="standard"
-            type="password"
-            name="password"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            error={formik.touched.password && !!formik.errors.password}
-            helperText={formik.touched.password && formik.errors.password}
-          />
-          <Button
-            variant="contained"
-            startIcon={<LoginIcon />}
-            type="submit"
-            form="signForm"
-            disabled={!(formik.isValid && formik.dirty)}
-          >
-            Login
-          </Button>
-        </form>
-      )}
-    </Formik>
+    <div className="background-image">
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+        validateOnChange
+        validateOnBlur
+      >
+        {(formik: any) => (
+          <div className="login-form-container">
+            <h1>WELCOME IN LIBRARY</h1>
+            <h2>PLEASE LOGIN</h2>
+            <form
+              className="login-form"
+              id="signForm"
+              onSubmit={formik.handleSubmit}
+              noValidate
+            >
+              <TextField
+                className="error-message"
+                id="username"
+                label="Username"
+                variant="standard"
+                name="username"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.username && !!formik.errors.username}
+                helperText={formik.touched.username && formik.errors.username}
+              />
+              <TextField
+                className="error-message"
+                id="password"
+                label="Password"
+                variant="standard"
+                type="password"
+                name="password"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.password && !!formik.errors.password}
+                helperText={formik.touched.password && formik.errors.password}
+              />
+              <Button
+                className="login-button"
+                variant="contained"
+                startIcon={<LoginIcon />}
+                type="submit"
+                form="signForm"
+                disabled={!(formik.isValid && formik.dirty)}
+                sx={{ marginTop: '1rem' }}
+              >
+                Login
+              </Button>
+            </form>
+            <button className="create-account-button">CREATE AN ACCOUNT</button>
+          </div>
+        )}
+      </Formik>
+    </div>
   );
 }
 
